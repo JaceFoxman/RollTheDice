78953e5dd2d9159aaa209e8ff66645459e751b44
Jason Permann <JaceFoxman@gmail.com>
2025-02-18 08:17:43 -0700

    Add RNG function

--- RollTheDice/RollTheDice.vb
+++ RollTheDice/RollTheDice.vb
@@ -3,10 +3,21 @@
 'RCET2265
 'Roll The Dice
 'https://github.com/JaceFoxman/RollTheDice.git
+Imports System.Text.RegularExpressions
+
 Module RollTheDice
 
     Sub Main()
 
     End Sub
+
+    Function RNG(min As Integer, max As Integer) As Integer
+        Dim value As Single
+        Randomize()
+        value = Rnd()
+        value *= max - min
+        value -= min
+        Return CInt(Math.Ceiling(value))
+    End Function
 
 End Module
