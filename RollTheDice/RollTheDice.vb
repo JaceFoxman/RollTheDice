c6828a3122fbd782d76101c3d8cd537e33033ee6
Jason Permann <JaceFoxman@gmail.com>
2025-02-20 09:00:57 -0700

    Add formating

--- RollTheDice/RollTheDice.vb
+++ RollTheDice/RollTheDice.vb
@@ -3,11 +3,32 @@
 'RCET2265
 'Roll The Dice
 'https://github.com/JaceFoxman/RollTheDice.git
+
+Option Strict On
+Option Explicit On
+Option Compare Text
 Imports System.Text.RegularExpressions
 
 Module RollTheDice
 
     Sub Main()
+        Dim randomNumber(12) As Integer
+
+        For i = 1 To 1000
+            randomNumber(RNG(1, 12)) += 1
+        Next
+
+        Console.WriteLine("Roll of the Dice".PadLeft(40))
+        Console.WriteLine(StrDup(80, "-"))
+
+        For i = 2 To UBound(randomNumber)
+            Console.Write($"{CStr(i).PadLeft(4)}  |")
+        Next
+        Console.WriteLine()
+
+        For i = 2 To UBound(randomNumber)
+            Console.Write($"{CStr(randomNumber(i)).PadLeft(4)}  |")
+        Next
 
     End Sub
 
@@ -16,7 +37,7 @@ Module RollTheDice
         Randomize()
         value = Rnd()
         value *= max - min
-        value -= min
+        value += min
         Return CInt(Math.Ceiling(value))
     End Function
 
